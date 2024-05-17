2d18cff35dc1f4bf19dae1f19123d71ef771aa80
Alyssa Wang <157160100+Alyssa-Wang12@users.noreply.github.com>
2024-05-17 11:42:56 -0600

    Add files via upload

---
+++ wordGuessingGame/wordGuessingGame/wordGuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordGuessingGameApp.swift
+//  wordGuessingGame
+//
+//  Created by Wang, Alyssa on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct wordGuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
